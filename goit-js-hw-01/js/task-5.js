dd703fd12b9693ecfe66db79e1ba4162a5842f53
AlexanderKudra89 <sasha.asnl@gmail.com>
2019-07-31 19:44:36 +0300

    HomeWork-01

--- goit-js-hw-01/js/task-5.js
+++ goit-js-hw-01/js/task-5.js
@@ -4,32 +4,27 @@ const chileCredits = 250;
 const australiaCredits = 170;
 const indiaCredits = 80;
 const jamaicaCredits = 120;
-const china = 'Китай';
-const chile = 'Чили';
-const australia = 'Австралия';
-const india = 'Индия';
-const jamaica = 'Ямайка';
 let message;
 
-switch (сontrySelection) {
-  case china:
-    message = `Доставка в ${china} будет стоить ${chinaCredits} кредитов.`;
+switch (сontrySelection.toLowerCase()) {
+  case 'китай':
+    message = `Доставка в Китай будет стоить ${chinaCredits} кредитов.`;
     break;
 
-  case chile:
-    message = `Доставка в ${chile} будет стоить ${chileCredits} кредитов.`;
+  case 'чили':
+    message = `Доставка в Чили будет стоить ${chileCredits} кредитов.`;
     break;
 
-  case australia:
-    message = `Доставка в ${australia} будет стоить ${australiaCredits} кредитов.`;
+  case 'австралия':
+    message = `Доставка в Австралию будет стоить ${australiaCredits} кредитов.`;
     break;
 
-  case india:
-    message = `Доставка в ${india} будет стоить ${indiaCredits} кредитов.`;
+  case 'индия':
+    message = `Доставка в Индию будет стоить ${indiaCredits} кредитов.`;
     break;
 
-  case jamaica:
-    message = `Доставка в ${jamaica} будет стоить ${jamaicaCredits} кредитов.`;
+  case 'ямайка':
+    message = `Доставка в Ямайку будет стоить ${jamaicaCredits} кредитов.`;
     break;
 
   default:
