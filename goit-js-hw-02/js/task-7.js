52f2b8763665f06b1c9f0727ccb8874598da949c
AlexanderKudra89 <sasha.asnl@gmail.com>
2019-08-07 21:42:48 +0300

    HomeWork-02

--- goit-js-hw-02/js/task-7.js
+++ goit-js-hw-02/js/task-7.js
@@ -5,10 +5,8 @@ let total = 0;
 do {
   input = prompt('Введите число');
   numbers.push(Number(input));
-
-  for (let number of numbers) {
-    total += number;
-  }
 } while (input !== null);
-
+for (let number of numbers) {
+  total += number;
+}
 console.log(`Общая сумма чисел равна [${total}]`);
