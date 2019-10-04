4bec8b8782936537f1f46cc05f4d36a5dfc9526d
AlexanderKudra89 <sasha.asnl@gmail.com>
2019-10-04 22:54:58 +0300

    HomeWork-07

--- goit-js-hw-07/js/task-8.js
+++ goit-js-hw-07/js/task-8.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const input = document.querySelector('#controls input');
+const render = document.querySelector('button[data-action="render"]');
+const destroy = document.querySelector('button[data-action="destroy"]');
+const newDiv = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  const boxSize = 30;
+  let step = 10;
+  let boxes = [];
+
+  function getNumber(min = 0, max = 255) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  for (let i = 0; i <= amount; i += 1) {
+    let box = document.createElement('div');
+    box.style.backgroundColor = `rgb(${getNumber()},${getNumber()},${getNumber()})`;
+    box.style.width = boxSize + i * step + 'px';
+    box.style.height = boxSize + i * step + 'px';
+    boxes.push(box);
+  }
+  return boxes;
+}
