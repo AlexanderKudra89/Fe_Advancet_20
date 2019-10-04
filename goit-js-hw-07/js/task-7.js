e4a3ad88d3b9577784b8f2492511df3a35f2b923
AlexanderKudra89 <sasha.asnl@gmail.com>
2019-10-04 23:02:39 +0300

    Homework-07

--- goit-js-hw-07/js/task-7.js
+++ goit-js-hw-07/js/task-7.js
@@ -3,8 +3,8 @@
 const fontSize = document.querySelector('#font-size-slider');
 const text = document.querySelector('#text');
 
-function changeFontSize(event) {
-  text.style.fontSize = event.currentTarget.value + 'px';
+function changeFontSize() {
+  text.style.fontSize = fontSize.value + 'px';
 }
 
 fontSize.addEventListener('input', changeFontSize);
