7b3fe4a9b48d400e954c99d512bf865032241fdc
AlexanderKudra89 <sasha.asnl@gmail.com>
2019-10-10 20:15:16 +0300

    Homework-7

--- goit-js-hw-07/js/task-6.js
+++ goit-js-hw-07/js/task-6.js
@@ -6,8 +6,10 @@ const length = input.getAttribute('data-length');
 const validationInput = function() {
   if (input.value.length === Number(length)) {
     input.classList.add('valid');
+    input.classList.replace('invalid', 'valid');
   } else {
     input.classList.add('invalid');
+    input.classList.replace('valid', 'invalid');
   }
 };
 
